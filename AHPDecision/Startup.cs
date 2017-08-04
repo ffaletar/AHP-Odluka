c7adcf9bfd5eac96fbfaf01dbd3a48471deaef87
ffaletar <ffaletar@foi.hr>
2017-08-04 20:23:52 +0200

    Add project files.

---
+++ AHPDecision/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AHPDecision.Startup))]
+namespace AHPDecision
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
